8766ebb2e5f019cde8ddcca33ee28582ac6815dc
zain <omarzain326@gmail.com>
2024-07-28 03:40:08 +0300

    deploy

--- utils/db.js
+++ utils/db.js
@@ -3,8 +3,9 @@ require('dotenv').config()
 const DB_HOST = process.env.DB_HOST
 const DB_PORT = process.env.DB_PORT
 const DB_NAME = process.env.DB_NAME
+const DB_CONNECT = process.env.DB_CONNECT
 
-mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`)
+mongoose.connect(DB_CONNECT)
 .then(()=>{
     console.log('mongoose successfuly connected to the database')
 })
